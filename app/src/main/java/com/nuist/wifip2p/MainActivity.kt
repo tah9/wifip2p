21154b655f65f913c4270c12b7946fded6c90f4d
18114157926 <690818882@qq.com>
2024-04-20 22:25:00 +0800

    备份

--- app/src/main/java/com/nuist/wifip2p/MainActivity.kt
+++ app/src/main/java/com/nuist/wifip2p/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nuist.wifip2p
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -10,6 +11,7 @@ import android.content.pm.PackageManager
 import android.net.wifi.p2p.WifiP2pConfig
 import android.net.wifi.p2p.WifiP2pDevice
 import android.net.wifi.p2p.WifiP2pDeviceList
+import android.net.wifi.p2p.WifiP2pGroup
 import android.net.wifi.p2p.WifiP2pInfo
 import android.net.wifi.p2p.WifiP2pManager
 import android.net.wifi.p2p.WifiP2pManager.Channel
@@ -36,10 +38,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import com.nuist.wifip2p.ui.theme.Wifip2pTheme
-import java.io.DataInputStream
-import java.io.DataOutputStream
-import java.net.ServerSocket
-import java.net.Socket
+
 
 var mIntent: Intent? = null
 
@@ -51,6 +50,7 @@ class MainActivity : ComponentActivity() {
 
     var mChannel: WifiP2pManager.Channel? = null
     var receiver: BroadcastReceiver? = null
+    val mContext: Activity = this
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,8 +59,7 @@ class MainActivity : ComponentActivity() {
             Wifip2pTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     MessageList(manager!!, mChannel!!)
                     Column(Modifier.fillMaxSize()) {
@@ -75,38 +74,11 @@ class MainActivity : ComponentActivity() {
             return
         }
 
-        mChannel = manager?.initialize(this, mainLooper, WifiP2pManager.ChannelListener {
-
-        })
-        manager?.removeGroup(mChannel, null)
-        mChannel = manager?.initialize(this, mainLooper, WifiP2pManager.ChannelListener {
-
-        })
-
-        mChannel?.also { channel ->
-            receiver = WiFiDirectBroadcastReceiver(channel, manager!!, this)
+        mChannel = manager?.initialize(this, mainLooper, null).apply {
+            receiver = WiFiDirectBroadcastReceiver(this!!, manager!!, mContext)
         }
 
-
-        // TODO: Consider calling
-        //    ActivityCompat#requestPermissions
-        // here to request the missing permissions, and then overriding
-        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-        //                                          int[] grantResults)
-        // to handle the case where the user grants the permission. See the documentation
-        // for ActivityCompat#requestPermissions for more details.
-//            return
-
-        manager?.discoverPeers(mChannel, object : WifiP2pManager.ActionListener {
-
-            override fun onSuccess() {
-                Log.e(TAG, "onSuccess: discoverPeers")
-            }
-
-            override fun onFailure(reasonCode: Int) {
-                Log.e(TAG, "onFailure: discoverPeers$reasonCode")
-            }
-        })
+        manager?.discoverPeers(mChannel, null)
 
     }
 
@@ -137,22 +109,17 @@ class MainActivity : ComponentActivity() {
 private const val TAG = "debug..."
 
 
-/**
- * A BroadcastReceiver that notifies of important Wi-Fi p2p events.
- */
-var serverSocket: ServerSocket? = null
 
 class WiFiDirectBroadcastReceiver(
     private val channel: Channel,
     private val manager: WifiP2pManager,
-    private val activity: MainActivity
+    private val activity: Activity
 ) : BroadcastReceiver() {
     private val peers = mutableListOf<WifiP2pDevice>()
 
     override fun onReceive(context: Context, intent: Intent) {
         mIntent = intent
-        var action: String? = intent.action
-        when (action) {
+        when (intent.action) {
             WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
                 // Check to see if Wi-Fi is enabled and notify appropriate activity
                 val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
@@ -172,11 +139,9 @@ class WiFiDirectBroadcastReceiver(
             WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
                 // Call WifiP2pManager.requestPeers() to get a list of current peers
                 if (ActivityCompat.checkSelfPermission(
-                        activity,
-                        Manifest.permission.ACCESS_FINE_LOCATION
+                        activity, Manifest.permission.ACCESS_FINE_LOCATION
                     ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
-                        activity,
-                        Manifest.permission.NEARBY_WIFI_DEVICES
+                        activity, Manifest.permission.NEARBY_WIFI_DEVICES
                     ) != PackageManager.PERMISSION_GRANTED
                 ) {
                     Log.d(TAG, "onReceive: PERMISSION_GRANTED")
@@ -200,59 +165,22 @@ class WiFiDirectBroadcastReceiver(
 
             WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
                 //此处状态检测触发频繁，要避免反复触发，做好标志位
-                manager.requestConnectionInfo(
-                    channel,
+                manager.requestConnectionInfo(channel,
                     object : WifiP2pManager.ConnectionInfoListener {
                         override fun onConnectionInfoAvailable(info: WifiP2pInfo) {
-                            var port: Int = 20112
-
-                            //组未搭建完毕，屏蔽
-                            if (info.groupFormed.not()) return
-
-                            Toast.makeText(context, "${info.isGroupOwner}", Toast.LENGTH_SHORT)
-                                .show()
+                            if (info.groupOwnerAddress == null) return
+                            Log.d(TAG, "onConnectionInfoAvailable: ${info}")
+                            val host = info.groupOwnerAddress.hostAddress
+                            Log.d(TAG, "onConnectionInfoAvailable: $host")
 
                             if (info.isGroupOwner) {
-                                // 该设备是群组的所有者
-
-                                //避免重复创建
-                                if (serverSocket!=null)return
-
-                                Thread {
-                                    serverSocket = ServerSocket(port)
-                                    Thread {
-                                        var accept = serverSocket?.accept()
-                                        var dos = DataOutputStream(accept?.getOutputStream())
-                                        dos.writeUTF(
-                                            "来自点对点"
-                                        )
-                                        dos.flush()
-                                    }.start()
-                                }.start()
+                                // 如果当前设备是群组所有者
                             } else {
-                                Log.d(TAG, "onConnectionInfoAvailable: ${info.groupOwnerAddress}")
-                                // 该设备是群组的成员，但不是所有者
-                                object : Thread() {
-                                    override fun run() {
-                                        var socket =
-                                            Socket(info.groupOwnerAddress, port)
-                                        object : Thread() {
-                                            override fun run() {
-                                                var inputStream =
-                                                    DataInputStream(socket.getInputStream())
-                                                var txt = inputStream.readUTF()
-                                                Log.d(TAG, "run: $txt")
-                                            }
-                                        }.start()
-                                    }
-                                }.start()
+                                // 如果当前设备是客户端
                             }
+                            return
                         }
                     })
-            }
-
-            WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
-                // Respond to this device's wifi state changing
             }
         }
     }
@@ -262,21 +190,18 @@ val wifiDevices = mutableStateListOf<WifiP2pDevice>()
 
 @Composable
 fun MessageList(
-    manager: WifiP2pManager,
-    channel: WifiP2pManager.Channel
+    manager: WifiP2pManager, channel: WifiP2pManager.Channel
 ) {
     val context = LocalContext.current
 
     LazyColumn {
         items(wifiDevices) { item ->
-            Text(
-                text = item.deviceName+"-"+item,
+            Text(text = "" + item,
                 modifier = Modifier
                     .padding(top = 10.dp, bottom = 10.dp)
                     .clickable {
                         attemptToConnect(item, context, manager, channel)
-                    }
-            )
+                    })
         }
     }
 }
@@ -298,8 +223,7 @@ fun connectDevicesList(
             Text(
                 text = item.deviceName,
                 color = Color.Green,
-                modifier = Modifier
-                    .padding(top = 10.dp, bottom = 10.dp)
+                modifier = Modifier.padding(top = 10.dp, bottom = 10.dp)
             )
         }
     }
@@ -312,24 +236,35 @@ fun attemptToConnect(
     manager: WifiP2pManager,
     channel: WifiP2pManager.Channel
 ) {
-    val config = WifiP2pConfig()
-    config.deviceAddress = device.deviceAddress
-    manager?.connect(
-        channel,
-        config,
-        object : WifiP2pManager.ActionListener {
-            override fun onSuccess() {
-                //success logic
-                Log.e(TAG, "onSuccess: logic${device.deviceName}")
-                connectDevices.add(device)
+    val targetConfig = WifiP2pConfig()
+    targetConfig.deviceAddress = device.deviceAddress
+    manager?.connect(channel, targetConfig, object : WifiP2pManager.ActionListener {
+
+        override fun onSuccess() {
+            WifiP2pDevice.AVAILABLE
+            //success logic
+            Log.e(TAG, "onSuccess: logic${device.deviceName}")
+            connectDevices.add(device)
+            try {
+                val group: WifiP2pGroup =
+                    mIntent!!.getParcelableExtra(WifiP2pManager.EXTRA_WIFI_P2P_GROUP)!!
+                for (wifiP2pDevice in group.clientList) {
+                    Log.d(TAG, "onSuccess: wifiP2pDevice=${wifiP2pDevice}")
+                    for (client in wifiDevices) {
+                        Log.d(TAG, "onSuccess: client=${client}")
+                    }
+                }
+            } catch (e: Exception) {
+                Log.d(TAG, "onSuccess: error=$e")
             }
 
-            override fun onFailure(reason: Int) {
-                //failure logic
-                Log.e(TAG, "onFailure: logic")
-            }
         }
-    )
+
+        override fun onFailure(reason: Int) {
+            //failure logic
+            Log.e(TAG, "onFailure: logic")
+        }
+    })
 }
 
 
